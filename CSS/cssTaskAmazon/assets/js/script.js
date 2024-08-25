15dd1864d6e03c83137d843c45e906a19a212960
ulvihuseynov <ulvihuseynov441@gmail.com>
2024-08-25 13:29:52 +0400

    newTask

---
+++ CSS/cssTaskAmazon/assets/js/script.js
@@ -0,0 +1,8 @@
+// function truncateText(element, maxLength) {
+//     if (element.innerText.length > maxLength) {
+//         element.innerText = element.innerText.substring(0, maxLength) + "...";
+//     }
+// }
+
+// const cards = document.querySelectorAll('.news-info');
+// cards.forEach(card => truncateText(card,50)); // 100 burada mətnin uzunluğunu təyin edir
--- cssNewstask/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-function truncateText(element, maxLength) {
-    if (element.innerText.length > maxLength) {
-        element.innerText = element.innerText.substring(0, maxLength) + "...";
-    }
-}
-
-const cards = document.querySelectorAll('.news-info');
-cards.forEach(card => truncateText(card,50)); // 100 burada mətnin uzunluğunu təyin edir
