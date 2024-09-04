f02b2980a0284aec20e01aaec13ecdfb67229f74
ulvihuseynov <ulvihuseynov441@gmail.com>
2024-09-04 09:42:04 +0400

    scss

--- SCSS/assets/js/menu.js
+++ SCSS/assets/js/menu.js
@@ -1,19 +1,25 @@
-const openDropdown=document.querySelectorAll(".dropdown");
-const dropdownList=document.querySelectorAll(".dropdown-list")
-openDropdown.forEach(openItem=>{
-    openItem.addEventListener("click",()=>{
-       dropdownList.forEach(list=>{
-        list.style.display="block"
-        console.log('ulvi');
+// const openDropdown=document.querySelectorAll(".dropdown");
+// const dropdownList=document.querySelectorAll(".dropdown-list")
+// openDropdown.forEach(openItem=>{
+//     openItem.addEventListener("click",()=>{
+//        dropdownList.forEach(list=>{
+//         list.style.display="block"
+//         console.log('ulvi');
         
-       })
-     })
-})
+//        })
+//      })
+// })
 
-openItem.removeEventListener("click",()=>{
-    dropdownList.forEach(list=>{
-     list.style.display="block"
-     console.log('ulvi');
+// openItem.removeEventListener("click",()=>{
+//     dropdownList.forEach(list=>{
+//      list.style.display="block"
+//      console.log('ulvi');
      
-    })
-  })
+//     })
+//   })
+
+const openMenu=document.querySelector(".open-menu");
+const mobileList=document.querySelector(".mobile-list")
+openMenu.addEventListener("click",()=>{
+mobileList.classList.toggle("toggle")
+})
